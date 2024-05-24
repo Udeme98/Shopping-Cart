34b56e1d1cb6a629b6b9590841a0a4c8ef6a4e0d
Udeme98 <Darealudeme1@gmail.com>
2024-05-24 07:16:47 +0100

    made updates to the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,11 @@ const Navbar = () => {
     <nav className="p-7 shadow-md bg-blue-500">
       <div className="container mx-auto flex items-center justify-between">
         <Link to={"/"}>
-          <h1 className="text-3xl uppercase cursor-pointer font-bold text-red-900 hover:text-red-500">
+          <h1 className="text-3xl uppercase cursor-pointer font-bold text-red-700 hover:text-red-500">
             Shopping Cart
           </h1>
         </Link>
-        <ul className="flex gap-3 uppercase text-xl font-bold text-red-900 ">
+        <ul className="flex gap-3 uppercase text-xl font-bold text-red-700 ">
           <Link to="/">
             <li className="cursor-pointer hover:text-red-500">Home</li>
           </Link>

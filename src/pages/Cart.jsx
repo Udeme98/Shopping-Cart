cb65c61ee69548f3f31412860ce3649d51b37de9
Udeme98 <Darealudeme1@gmail.com>
2024-05-25 20:22:26 +0100

    cart page setup

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
   const [total, setTotal] = useState(0);
@@ -13,7 +14,18 @@ const Cart = () => {
 
   return (
     <>
-      {cart && cart.length ? <div>Hello</div> : <div>Your Cart is Empty</div>}
+      {cart && cart.length ? (
+        <div>Hello</div>
+      ) : (
+        <div className="min-h-[80vh] flex justify-center items-center flex-col">
+          <p className="text-3xl font-semibold">Your Cart is Empty</p>
+          <Link to={"/"}>
+            <button className="text-sm p-3 mt-5 px-8 rounded-lg uppercase font-medium tracking-wider inline-block shadow-md bg-black text-white hover:bg-gray-500">
+              Shop Now!
+            </button>
+          </Link>
+        </div>
+      )}
     </>
   );
 };

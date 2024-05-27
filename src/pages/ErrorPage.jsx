fb69f7d3805754d086f96d1e90cfe4a2a4ae9afd
Udeme98 <Darealudeme1@gmail.com>
2024-05-27 06:31:26 +0100

    error page: homepage button added

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
   return (
     <div className="min-h-[80vh] flex justify-center items-center flex-col">
       <h1 className="text-5xl font-semibold">404</h1>
       <p className="text-3xl ">This page does not exist.</p>
+      <Link to={"/"}>
+        <button className="text-sm p-3 mt-5 px-8 rounded-lg uppercase font-medium tracking-wider inline-block shadow-md bg-black text-white hover:bg-gray-500">
+          Shop Now!
+        </button>
+      </Link>
     </div>
   );
 };

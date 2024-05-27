4e4eb333dd6287aec53e45c900780380828b7d32
Udeme98 <Darealudeme1@gmail.com>
2024-05-27 06:26:18 +0100

    error page setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Homepage from "./pages/Homepage";
 import Navbar from "./components/Navbar";
 import Cart from "./pages/Cart";
 import CartDetails from "./pages/CartDetails";
+import ErrorPage from "./pages/ErrorPage";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/" element={<Homepage />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/cart-details/:id" element={<CartDetails />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </div>
   );
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,9 @@
+const ErrorPage = () => {
+  return (
+    <div>
+      <h1>404</h1>
+      <p>This page does not exist.</p>
+    </div>
+  );
+};
+export default ErrorPage;

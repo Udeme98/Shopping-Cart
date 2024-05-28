a678b39ff9e4a692b08eb181ad61b9050d4372e5
Udeme98 <Darealudeme1@gmail.com>
2024-05-28 13:20:35 +0100

    working on the cartDetails page: styling

--- src/pages/CartDetails.jsx
+++ src/pages/CartDetails.jsx
@@ -7,14 +7,18 @@ const CartDetails = () => {
   const { id } = useParams();
   //   console.log(id);
   const singleProduct = products[`${id - 1}`];
-  console.log(singleProduct);
+  //   console.log(singleProduct);
 
   return (
-    <div>
-      <div>
-        <img src={singleProduct.image} alt={singleProduct.title} />
+    <div className="flex container mx-auto gap-5 p-3 min-h-[80vh] items-center max-w-[70%]">
+      <div className="flex-1">
+        <img
+          src={singleProduct.image}
+          alt={singleProduct.title}
+          className="w-96"
+        />
       </div>
-      <div>
+      <div className="flex flex-col gap-5 text-2xl flex-1">
         <h2>Name: {singleProduct.title}</h2>
         <h2>Description: {singleProduct.description}</h2>
         <h2>Price: ${singleProduct.price}</h2>

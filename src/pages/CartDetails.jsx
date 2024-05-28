fb74183efd7f64ce884e0360892aef5bfc6864ea
Udeme98 <Darealudeme1@gmail.com>
2024-05-28 12:00:47 +0100

    working on the cartDetails page

--- src/pages/CartDetails.jsx
+++ src/pages/CartDetails.jsx
@@ -1,9 +1,13 @@
 import { useParams } from "react-router-dom";
+import { products } from "../products";
+import { useState } from "react";
 
 const CartDetails = () => {
+  const [productDetails, setProductDetails] = useState(null);
   const { id } = useParams();
   //   console.log(id);
+  console.log(products[`${id - 1}`]);
 
-  return <div>Cart Details Page {id}</div>;
+  return <div>Product {id}</div>;
 };
 export default CartDetails;

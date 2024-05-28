a4a28d2547de41a9340a477d2034e5f641818559
Udeme98 <Darealudeme1@gmail.com>
2024-05-28 13:07:38 +0100

    working on the cartDetails page

--- src/pages/CartDetails.jsx
+++ src/pages/CartDetails.jsx
@@ -3,11 +3,23 @@ import { products } from "../products";
 import { useState } from "react";
 
 const CartDetails = () => {
-  const [productDetails, setProductDetails] = useState(null);
+  //   const [productDetails, setProductDetails] = useState(null);
   const { id } = useParams();
   //   console.log(id);
-  console.log(products[`${id - 1}`]);
+  const singleProduct = products[`${id - 1}`];
+  console.log(singleProduct);
 
-  return <div>Product {id}</div>;
+  return (
+    <div>
+      <div>
+        <img src={singleProduct.image} alt={singleProduct.title} />
+      </div>
+      <div>
+        <h2>Name: {singleProduct.title}</h2>
+        <h2>Description: {singleProduct.description}</h2>
+        <h2>Price: ${singleProduct.price}</h2>
+      </div>
+    </div>
+  );
 };
 export default CartDetails;

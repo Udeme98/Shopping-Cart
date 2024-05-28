529540cbbfb6968bcbe7381e15cceaf7a5dbb167
Udeme98 <Darealudeme1@gmail.com>
2024-05-28 15:12:20 +0100

    CartDetails updated

--- src/components/CartItems.jsx
+++ src/components/CartItems.jsx
@@ -1,34 +1,34 @@
 import { useEffect, useState } from "react";
 import CartItem from "./CartItem";
 import { FaSpinner } from "react-icons/fa";
-import { products } from "../products";
+// import { products } from "../products";
 
 const CartItems = () => {
-  // const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  // useEffect(() => {
-  //   const fetchProducts = async () => {
-  //     setLoading(true);
-  //     try {
-  //       const res = await fetch("http://fakestoreapi.com/products");
-  //       const data = await res.json();
-  //       if (data) {
-  //         console.log(data);
-  //         setLoading(false);
-  //         setProducts(data);
-  //       }
-  //     } catch (error) {
-  //       setLoading(false);
-  //       console.log(error);
-  //     }
-  //   };
-  //   fetchProducts();
-  // }, []);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch("http://fakestoreapi.com/products");
+        const data = await res.json();
+        if (data) {
+          console.log(data);
+          setLoading(false);
+          setProducts(data);
+        }
+      } catch (error) {
+        setLoading(false);
+        console.log(error);
+      }
+    };
+    fetchProducts();
+  }, []);
 
   return (
     <div>
-      {/* {loading ? (
+      {loading ? (
         <div className="min-h-screen flex justify-center items-center">
           <FaSpinner className="text-6xl animate-spin ease-in-out " />
         </div>
@@ -40,13 +40,13 @@ const CartItems = () => {
               })
             : null}
         </div>
-      )} */}
+      )}
 
-      <div className="flex container mx-auto gap-5 p-3 flex-wrap">
+      {/* <div className="flex container mx-auto gap-5 p-3 flex-wrap">
         {products.map((item) => {
           return <CartItem item={item} key={item.id} />;
         })}
-      </div>
+      </div> */}
     </div>
   );
 };
--- src/pages/CartDetails.jsx
+++ src/pages/CartDetails.jsx
@@ -1,33 +1,41 @@
 import { useParams } from "react-router-dom";
-import { products } from "../products";
-import { useState } from "react";
+// import { products } from "../products";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart, removeFromCart } from "../cartSlice";
 
 const CartDetails = () => {
-  //   const [productDetails, setProductDetails] = useState(null);
+  const [productDetails, setProductDetails] = useState(null);
   const { id } = useParams();
-  //   console.log(id);
-  const singleProduct = products[`${id - 1}`];
-  //   console.log(singleProduct);
 
   const { cart } = useSelector((store) => store);
   const dispatch = useDispatch();
 
   const handleAddToCart = () => {
-    dispatch(addToCart(singleProduct));
+    dispatch(addToCart(productDetails));
   };
 
   const handleRemoveFromCart = () => {
-    dispatch(removeFromCart(singleProduct.id));
+    dispatch(removeFromCart(productDetails.id));
   };
+
+  useEffect(() => {
+    const fetchProductDetails = async () => {
+      const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+      const data = await res.json();
+      if (data) {
+        setProductDetails(data);
+      }
+    };
+    fetchProductDetails();
+  }, []);
 
   return (
     <div className="flex container mx-auto gap-5 p-3 min-h-[80vh] items-center max-w-[70%] flex-col lg:flex-row">
       <div className="flex-1">
         <img
-          src={singleProduct.image}
-          alt={singleProduct.title}
+          src={productDetails?.image}
+          alt={productDetails?.title}
           className="w-96"
         />
       </div>
@@ -35,30 +43,30 @@ const CartDetails = () => {
         <h2 className="font-bold text-gray-700">
           Name:{" "}
           <span className="font-semibold text-gray-500">
-            {singleProduct.title}
+            {productDetails?.title}
           </span>
         </h2>
         <h2 className="font-bold text-gray-700">
           Description:{" "}
           <span className="font-semibold text-gray-500">
-            {singleProduct.description}
+            {productDetails?.description}
           </span>
         </h2>
         <h2 className="font-bold text-gray-700">
           Price:{" "}
           <span className="font-semibold text-gray-500">
-            ${singleProduct.price}
+            ${productDetails?.price}
           </span>
         </h2>
         <button
           onClick={
-            cart.find((product) => product.id === singleProduct.id)
+            cart.find((product) => product.id === productDetails.id)
               ? handleRemoveFromCart
               : handleAddToCart
           }
           className="text-sm p-4 mt-5 rounded-lg uppercase font-medium tracking-wider inline-block shadow-md bg-black text-white hover:bg-gray-500"
         >
-          {cart.find((product) => product.id === singleProduct.id)
+          {cart.find((product) => product.id === productDetails.id)
             ? "Remove from cart"
             : "Add to Cart"}
         </button>
@@ -67,3 +75,13 @@ const CartDetails = () => {
   );
 };
 export default CartDetails;
+
+// const productDetails = products[`${id - 1}`];
+//   console.log(productDetails);
+//  const handleAddToCart = () => {
+//    dispatch(addToCart(productDetails));
+//  };
+
+//  const handleRemoveFromCart = () => {
+//    dispatch(removeFromCart(productDetails.id));
+//  };

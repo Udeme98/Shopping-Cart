85bed1ae56d6dd62eff548fd866ee9f7c2de9b65
Udeme98 <Darealudeme1@gmail.com>
2024-05-30 09:44:39 +0100

    Navbar mobile responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,12 +35,30 @@ const Navbar = () => {
           className="md:hidden"
         >
           {menuOpen ? (
-            <FaTimes className="font-bold text-red-800 text-4xl cursor-pointer hover:text-red-500  transition duration-300" />
+            <FaTimes className="font-bold text-red-600 text-4xl cursor-pointer hover:text-red-500  transition duration-300" />
           ) : (
-            <FaBars className="font-bold text-red-800 text-4xl cursor-pointer hover:text-red-500  transition duration-300" />
+            <FaBars className="font-bold text-red-600 text-4xl cursor-pointer hover:text-red-500  transition duration-300" />
           )}
         </button>
       </div>
+
+      {menuOpen ? (
+        <div className="flex flex-col space-y-3 font-bold mt-4 text-4xl text-red-600 items-center  md:hidden">
+          <Link to={"/"} onClick={() => setMenuOpen(false)}>
+            <FaHome
+              className="cursor-pointer hover:text-red-500  transition duration-300"
+              title="Home"
+            />
+          </Link>
+          <Link to={"/cart"} onClick={() => setMenuOpen(false)}>
+            {" "}
+            <FaShoppingCart
+              className="cursor-pointer hover:text-red-500 transition duration-300"
+              title="Cart"
+            />
+          </Link>
+        </div>
+      ) : null}
     </nav>
   );
 };

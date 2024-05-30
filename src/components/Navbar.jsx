757631aab6fb9a763bcfd3d93b1636832cd3cb3d
Udeme98 <Darealudeme1@gmail.com>
2024-05-30 09:01:42 +0100

    Navbar mobile responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,45 @@
 import { Link } from "react-router-dom";
 
-import { FaHome, FaShoppingCart, FaBars } from "react-icons/fa";
+import { useState } from "react";
+import { FaHome, FaShoppingCart, FaBars, FaTimes } from "react-icons/fa";
+
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <nav className="p-7 shadow-md bg-blue-500">
+    <nav className="p-7 shadow-md bg-blue-400">
       <div className="container mx-auto flex items-center justify-between">
         <Link to={"/"}>
-          <h1 className="text-3xl uppercase cursor-pointer font-bold text-red-700 hover:text-red-500">
+          <h1 className="text-3xl uppercase cursor-pointer font-bold text-red-600 hover:text-red-500 transition duration-300">
             Shoppier
           </h1>
         </Link>
-        <ul className="hidden md:flex gap-3 uppercase  font-bold text-red-800 text-4xl items-center">
+
+        <ul className="hidden md:flex gap-3 uppercase  font-bold text-red-600 text-4xl items-center">
           <Link to="/">
             <FaHome
-              className="cursor-pointer hover:text-red-500 "
+              className="cursor-pointer hover:text-red-500  transition duration-300"
               title="Home"
             />
           </Link>
           <Link to="/cart">
             <FaShoppingCart
-              className="cursor-pointer hover:text-red-500"
+              className="cursor-pointer hover:text-red-500 transition duration-300"
               title="Cart"
             />
           </Link>
         </ul>
-        <FaBars className="font-bold text-red-800 text-4xl cursor-pointer hover:text-red-500 md:hidden" />
+
+        <button
+          onClick={() => setMenuOpen((prev) => !prev)}
+          className="md:hidden"
+        >
+          {menuOpen ? (
+            <FaTimes className="font-bold text-red-800 text-4xl cursor-pointer hover:text-red-500  transition duration-300" />
+          ) : (
+            <FaBars className="font-bold text-red-800 text-4xl cursor-pointer hover:text-red-500  transition duration-300" />
+          )}
+        </button>
       </div>
     </nav>
   );

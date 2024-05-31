5c5625efe172269aca2487fbff0b0ece9454ef65
Udeme98 <Darealudeme1@gmail.com>
2024-05-31 10:35:17 +0100

    Dark mode: created context api to pass down state function

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,25 @@ import Navbar from "./components/Navbar";
 import Cart from "./pages/Cart";
 import CartDetails from "./pages/CartDetails";
 import ErrorPage from "./pages/ErrorPage";
+import { createContext, useState } from "react";
+
+export const DarkModeContext = createContext(false);
 
 const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/cart-details/:id" element={<CartDetails />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-    </div>
+    <DarkModeContext.Provider value={{ darkMode, setDarkMode }}>
+      <div className={`${darkMode && "dark"}`}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/cart-details/:id" element={<CartDetails />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </div>
+    </DarkModeContext.Provider>
   );
 };
 export default App;

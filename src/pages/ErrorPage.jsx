a901753cf78c27536a0763bcad899419383d451e
Udeme98 <Darealudeme1@gmail.com>
2024-06-02 14:17:06 +0100

    Dark mode: ErrorPage(404) section

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -2,14 +2,20 @@ import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <div className="min-h-[80vh] flex justify-center items-center flex-col font-poppins">
-      <h1 className="text-5xl font-semibold">404</h1>
-      <p className="text-3xl ">This page does not exist.</p>
-      <Link to={"/"}>
-        <button className="text-sm p-3 mt-5 px-8 rounded-lg uppercase font-medium tracking-wider inline-block shadow-md bg-black text-white hover:bg-gray-500">
-          Shop Now!
-        </button>
-      </Link>
+    <div className="min-h-screen dark:bg-slate-800">
+      <div className="h-[80vh] flex justify-center items-center flex-col font-poppins">
+        <h1 className="text-6xl font-semibold text-slate-700 dark:text-slate-200">
+          404
+        </h1>
+        <p className="text-3xl text-slate-700 dark:text-slate-200">
+          This page does not exist.
+        </p>
+        <Link to={"/"}>
+          <button className="text-sm p-3 mt-5 px-8 rounded-lg uppercase font-medium tracking-wider inline-block shadow-md bg-slate-700 text-slate-100 hover:bg-slate-600">
+            Shop Now!
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
